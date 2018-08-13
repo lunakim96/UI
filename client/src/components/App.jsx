cd751e56e9f0a9b249a3f398d7beb71582b47ed8
Luna Kim <nayoungkimm@gmail.com>
2018-08-12 21:31:14 -0700

    removed css files

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import mainView from './mainView.jsx';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            movies: [],
+            searchQuery: '',
+            movieList: [],
+            selectedMovies: []          ,
         };
     }
     render() {  
         return (
             <div>
+            <mainView />
             <h1>Title</h1>
             <p>Hello World!</p>
             </div>
---
+++ client/src/components/mainView.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const mainView = ({}) => {
+    <div>
+        <img src="https://images.unsplash.com/photo-1512204231446-d520ee0a0873?ixlib=rb-0.3.5&s=d7e392f79701cfe02f92f9e97d8b0bde&auto=format&fit=crop&w=1652&q=80" />
+    </div>
+}
+export default mainView;
